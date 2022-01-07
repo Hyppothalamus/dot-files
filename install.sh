ffdee808d1aa25829f8c97a65d371b021331d229
Kasper Haepers <kasper.haepers@outlook.be>
2022-01-07 15:13:21 +0100

    mistake in install

--- install.sh
+++ install.sh
@@ -17,7 +17,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-fish -C fisher install IlanCosman/tide@v5 && exit
+fish -c fisher install IlanCosman/tide@v5 && exit
 
 echo "moving stuff"
 
