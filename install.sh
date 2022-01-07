05f5fe9dc0883a9a1b320872cba805c66713f3d5
Kasper Haepers <kasper.haepers@outlook.be>
2022-01-07 14:10:25 +0100

    small basic deb install script + fish fisher and tide stuff

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+echo "installing packages"
+
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt install -y fish fortune cowsay lolcat tmux neovim
+
+echo "installing plugin managers"
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fisher install IlanCosman/tide@v5
+
+echo "moving stuff"
+
+cp openTmux.sh /opt/openTmux.sh
+chmod 755 /opt/openTmux.sh
+ln -s /opt/openTmux.sh /usr/bin/openTmux
+
+echo "installed opentmux script"
