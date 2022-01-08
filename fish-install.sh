4ddaff34750aef2292c28e5a998f3602a1f24276
Kasper Haepers <kasper.haepers@outlook.be>
2022-01-08 14:31:56 +0100

    removed files from fish and no auto config for tide :sadge:

--- fish-install.sh
+++ fish-install.sh
@@ -4,4 +4,5 @@ echo "installing fisher and tide"
 curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 
 fisher install IlanCosman/tide@v5
-echo "done"
+echo "done now configuring tide"
+tide configur
