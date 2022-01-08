f507a64028a55dd175f796277b270a86853bc063
Kasper Haepers <kasper.haepers@outlook.be>
2022-01-08 10:12:03 +0100

    better fisher install

--- install.sh
+++ install.sh
@@ -12,12 +12,12 @@ sudo apt update
 sudo apt install -y curl fish fortune cowsay lolcat tmux neovim
 
 echo "installing plugin managers"
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fish -c (curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher && exit)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-fish -c fisher install IlanCosman/tide@v5 && exit
+fish -c (fisher install IlanCosman/tide@v5 && exit)
 
 echo "moving stuff"
 
