6495e8be568449ccc22d98d6af1017744058a4d6
Kasper Haepers <kasper.haepers@outlook.be>
2022-01-09 11:29:47 +0100

    i am retard and messed up shell script

--- fish-install.sh
+++ fish-install.sh
@@ -6,4 +6,4 @@ curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher install IlanCosman/tide@v5
 echo "done now configuring tide"
 runuser -l $SUDO_USER -c 'tide configure'
-runuser -l $SUDO_USER -c 'chsh -s /bin/fish'
+runuser -l $SUDO_USER -c 'chsh -s /usr/bin/fish'
