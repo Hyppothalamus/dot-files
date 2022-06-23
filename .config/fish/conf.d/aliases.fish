37dc9625cc0d17921f826596a0098ec9bdf8b248
Kasper Haepers <kasper.haepers@outlook.be>
2022-06-23 23:00:36 +0200

    sxhkd qwerty stuffs also start polybar redo and some other stuff

--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -5,6 +5,7 @@ alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 alias ll="exa -la --icons"
 alias ls="exa --icons"
 alias emacs='emacsclient -c -a "emacs"'
+alias neo='neo -D'
 
 function cdls 
     cd $argv; ll
