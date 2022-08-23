43b5a20a0daaba3a95d545e1c2799924a186d5dc
Kasper Haepers <kasper.haepers@outlook.be>
2022-08-23 14:37:59 +0200

    i am retared with nvim lua xD

---
+++ .config/nvim/lua/custom_tree.lua
@@ -0,0 +1,97 @@
+require'nvim-tree'.setup {
+  disable_netrw       = true,
+  hijack_netrw        = true,
+  open_on_setup       = true,
+  ignore_ft_on_setup  = {},
+  open_on_tab         = false,
+  hijack_cursor       = false,
+  update_cwd          = false,
+  respect_buf_cwd     = true,
+  create_in_closed_folder = false,
+  hijack_directories   = {
+    enable = true,
+    auto_open = true,
+  },
+  diagnostics = {
+    enable = false,
+    icons = {
+      hint = "",
+      info = "",
+      warning = "",
+      error = "",
+    }
+  },
+  update_focused_file = {
+    enable      = true,
+    update_cwd  = false,
+    ignore_list = {}
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
+  },
+  filters = {
+    dotfiles = false,
+    custom = {}
+  },
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = 'left',
+    mappings = {
+      custom_only = false,
+      list = {}
+    }
+  },
+  actions = {
+      open_file = {
+          quit_on_open = false,
+          resize_window = false,
+      }
+    },
+    renderer = {
+        indent_markers = {
+            enable = true,
+        },
+        icons = {
+            padding = ' ',
+            symlink_arrow = ' >> ',
+            show = {
+                git = true,
+                folder = true,
+                file = true,
+                folder_arrow = true,
+            },
+            glyphs = {
+                default = '',
+                symlink = '',
+                git = {
+                    unstaged = "✗",
+                    staged = "✓",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "★",
+                    deleted = "",
+                    ignored = "◌",
+                },
+                folder = {
+                    arrow_open = "",
+                    arrow_closed = "",
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                },
+            },
+        },
+        highlight_git = true,
+        highlight_opened_files = "all",
+        root_folder_modifier = ':~',
+        add_trailing = true,
+        group_empty = true,
+        special_files = { "README.md", "Makefile", "MAKEFILE" },
+    }
+}
