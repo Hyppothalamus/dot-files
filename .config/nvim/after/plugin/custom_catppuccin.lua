d0aa6852418acedd454755d5baba572789f0d600
Kasper Haepers <kasper.haepers@outlook.be>
2022-10-04 16:22:08 +0200

    hihi

--- .config/nvim/after/plugin/custom_catppuccin.lua
+++ .config/nvim/after/plugin/custom_catppuccin.lua
@@ -8,5 +8,5 @@ end
 catppuccin.setup({
     transparent_background = true,
     })
-vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+vim.g.catppuccin_flavour = "macchiato" -- latte, frappe, macchiato, mocha
 vim.cmd[[colorscheme catppuccin]]
