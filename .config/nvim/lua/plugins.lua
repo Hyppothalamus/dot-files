5de0b3aef6c05f36f37b260afc99b19b1287e057
Kasper Haepers <kasper.haepers@outlook.be>
2022-11-09 13:28:51 +0100

    plugins layout

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -8,33 +8,33 @@ end
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'andweeb/presence.nvim'
-  use {'neoclide/coc.nvim', branch = 'release'}
-  use 'wakatime/vim-wakatime'
-  use {'catppuccin/nvim', as = 'catppuccin'}
-  use 'nvim-lualine/lualine.nvim'
-  use {
-  'kyazdani42/nvim-tree.lua',
-  as = 'nvim-tree',
-  requires = {
-    'kyazdani42/nvim-web-devicons', -- optional, for file icons
-  },
-  tag = 'nightly' -- optional, updated every week. (see issue #1193)
-}
-  use 'kyazdani42/nvim-web-devicons'
-  use 'mhinz/vim-signify'
-  use {"akinsho/toggleterm.nvim", as = 'toggleterm', tag = 'v2.*'}
-  use {
-      "nvim-telescope/telescope.nvim", tag = '0.1.0',
-      requires = { {"nvim-lua/plenary.nvim"} }, as = "telescope"
-  }
-  use {
-      "startup-nvim/startup.nvim",
-      requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
-      as = "startup"
-  }
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use 'neovim/nvim-lspconfig'
+    use 'andweeb/presence.nvim'
+    use {'neoclide/coc.nvim', branch = 'release'}
+    use 'wakatime/vim-wakatime'
+    use {'catppuccin/nvim', as = 'catppuccin'}
+    use 'nvim-lualine/lualine.nvim'
+    use {
+    'kyazdani42/nvim-tree.lua',
+    as = 'nvim-tree',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icons
+    },
+    tag = 'nightly' -- optional, updated every week. (see issue #1193)
+    }
+    use 'kyazdani42/nvim-web-devicons'
+    use 'mhinz/vim-signify'
+    use {"akinsho/toggleterm.nvim", as = 'toggleterm', tag = 'v2.*'}
+    use {
+        "nvim-telescope/telescope.nvim", tag = '0.1.0',
+        requires = { {"nvim-lua/plenary.nvim"} }, as = "telescope"
+    }
+    use {
+        "startup-nvim/startup.nvim",
+        requires = {"nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim"},
+        as = "startup"
+    }
 
 end)
