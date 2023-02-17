1d6e821df24f89f82a3d5f720f79fc395b267e93
Kasper Haepers <kasper.haepers@outlook.be>
2023-02-17 14:14:32 +0100

    bufferline styling because moved away from barbar

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -6,19 +6,39 @@ if not ok then
 end
 
 local macchiato = require("catppuccin.palettes").get_palette "macchiato"
-print(macchiato)
 bufferline.setup({
     options = {
         mode = 'tabs',
-        diagnostic = 'nvim-lsp'
+        diagnostic = 'nvim-lsp',
+        separator_style = 'slant',
+        color_icons = false,
+        show_buffer_icons = false,
+        show_buffer_default_icon = false,
+        offsets = {
+            {
+                filetype = 'NvimTree',
+                text = 'File Explorer',
+                highlight = false,
+                separator = false
+            }
+        },
     },
     highlights = require("catppuccin.groups.integrations.bufferline").get {
         custom = {
             all = {
                 fill = {
-                    bg = macchiato.base,
+                    bg = macchiato.base
+                },
+                separator = {
                     fg = macchiato.base
-                }
+                },
+                separator_selected = {
+                    fg = macchiato.base
+                },
+                pick_visible = {
+                    bg = macchiato.text,
+                    fg = macchiato.text,
+                },
             },
         }
     }
